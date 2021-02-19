8a589ce72e793f68e9b55bbe4857c3c057b37699
Mohamed Lemine Ould El-Hadj <mohamedlemineelhadj@outlook.com>
2021-02-19 11:11:46 -0500

    Basic tcp server that listens on port 9000

--- server.cpp
+++ server.cpp
@@ -2,6 +2,37 @@
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    struct sockaddr_in address;
+    address.sin_port = PORT;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    int address_length = sizeof(address);
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd == 0)
+    {
+        std::cout << "Unable to create socket" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if ((bind(sockfd, (struct sockaddr*)(&address), address_length) < 0))
+    {
+        std::cout << "Unable to bind" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    
+    if (listen(sockfd, 8) < 0) // Maximum connections is 8
+    {
+        std::cout << "Unable to listen" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    int new_socket = accept(sockfd, (sockaddr *)(&address), (socklen_t *)&address_length);
+    if(new_socket < 0)
+    {
+        std::cout << "Unable to accept new connection" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
     return 0;
 }
--- server.h
+++ server.h
@@ -2,6 +2,11 @@
 #define SERVER_H
 
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstdlib>
+
+#define PORT 9000
 
 #endif /* SERVER_H */
 
