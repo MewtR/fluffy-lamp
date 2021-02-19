3a68a601d9513b2679403ff67cecc023c632cde4
Mohamed Lemine Ould El-Hadj <mohamedlemineelhadj@outlook.com>
2021-02-19 11:23:08 -0500

    Print received message and send response

--- server.cpp
+++ server.cpp
@@ -1,9 +1,13 @@
 #include "server.h"
+#include <unistd.h>
 
 int main()
 {
+    int received;
+    char buffer[1024] = {0};
+    std::string msg = "Hello from server";
     struct sockaddr_in address;
-    address.sin_port = PORT;
+    address.sin_port = htons(PORT);
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     int address_length = sizeof(address);
@@ -33,6 +37,12 @@ int main()
         std::cout << "Unable to accept new connection" << std::endl;
         exit(EXIT_FAILURE);
     }
+
+    received = read(new_socket, buffer, 1024);
+    std::cout << "Message received: " << std::endl;
+    printf("%s\n", buffer);
+    send(new_socket, msg.c_str(), msg.length(), 0);
+    std::cout << "Replied" << std::endl;
 
     return 0;
 }
--- server.h
+++ server.h
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <cstdlib>
+#include <unistd.h>
 
 #define PORT 9000
 
